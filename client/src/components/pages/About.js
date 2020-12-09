36116a8c7d8ff9bd3ff454eac075b994f5d56492
Ankush Kumar <ankush@Ankushs-MacBook-Pro.local>
2020-12-10 02:44:21 +0530

    API CREATED

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div>
+            <h1>About This App</h1>
+            <p className="my-1"> 
+                This is A copied Full stack react Application for keeping Contacts
+            </p>
+        </div>
+    )
+}
+
+export default About
